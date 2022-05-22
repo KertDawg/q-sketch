25ad945fc7abb2fb899595a935207998662bba2f
Kertis Henderson <kertishenderson@gmail.com>
2022-05-22 13:24:25 -0400

    Fixed packages.

--- ui/src/components/Component.js
+++ ui/src/components/Component.js
@@ -1,13 +1,14 @@
-import { h } from 'vue'
-import { QBadge } from 'quasar'
+import { h } from "vue"
+import { QBadge } from "quasar"
+
 
 export default {
-  name: 'Sketch',
+  name: "Sketch",
 
   setup () {
     return () => h(QBadge, {
-      class: 'Sketch',
-      label: 'Sketch'
+      class: "Sketch",
+      label: "Sketch"
     })
   }
-}
+};
